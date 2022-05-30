20001248a123da31942d46ed7078104637f7e1d6
PrantoMondal <prantomondal517@gmail.com>
2022-05-30 13:22:27 +0600

    Click Event added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,16 +11,40 @@ class HomePage extends StatelessWidget {
         title: Text('My App'),
       ),
       body: Center(
-        child: Container(
-          padding: EdgeInsets.all(12),
-          decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-            //borderRadius: BorderRadius.circular(12)
-            shape: BoxShape.circle
-          ),
-          child: Text(
-            'Click',
-            style: TextStyle(color: Colors.white),
+        child: GestureDetector(
+          onTap: (){
+            print('Clicked');
+          },
+          child: Container(
+            /*transform: Matrix4.rotationZ(0.01745*45),
+            transformAlignment: Alignment.center,*/
+            alignment: Alignment.center,
+            width: 70,
+            height: 70,
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.lightBlue,
+                  offset: Offset(0,0),
+                  blurRadius: 10,
+                  spreadRadius: 2
+                )
+              ],
+              gradient: LinearGradient(
+                colors: [Theme.of(context).primaryColor,
+                  Theme.of(context).primaryColor.withOpacity(0.5)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter
+              )
+            ),
+            child: Text(
+              'Click',
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
       ),
